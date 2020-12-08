e00336b6637492e9c681422597b1af4ec93a07fa
caito <caitocd@gmail.com>
2020-12-08 20:26:37 -0300

    Inicio Proyecto

---
+++ src/main/java/com/caito/crudproductos/CrudProductosApplication.java
@@ -0,0 +1,13 @@
+package com.caito.crudproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caito/crudproductos/CrudProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caito.crudproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
